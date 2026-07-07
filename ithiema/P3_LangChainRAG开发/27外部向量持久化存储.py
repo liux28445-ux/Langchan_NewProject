37800d2776b8cb7878116f52ff632cbaa0b13d7a
菠菜菜菜菜菜 <liux28445@gmail.com>
2026-07-07 20:31:30 +0800

    1

--- ithiema/P3_LangChainRAG开发/27外部向量持久化存储.py
+++ ithiema/P3_LangChainRAG开发/27外部向量持久化存储.py
@@ -3,7 +3,7 @@ from langchain_community.embeddings import DashScopeEmbeddings
 from langchain_community.document_loaders import CSVLoader
 
 # Chroma 向量数据库（轻量级的）
-# 确保 langchain-chroma chromadb 这两个库安装了的，没有的话请pip install
+# 确保 langchain-chroma.yml chromadb 这两个库安装了的，没有的话请pip install
 
 vector_store = Chroma(
     collection_name="test",     # 当前向量存储起个名字，类似数据库的表名称
