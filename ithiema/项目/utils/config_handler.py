e39296a97569f8c2eb0bd8abcf808679fd6ff189
菠菜菜菜菜菜 <liux28445@gmail.com>
2026-07-09 21:02:59 +0800

    Initial commit

--- ithiema/项目/utils/config_handler.py
+++ ithiema/项目/utils/config_handler.py
@@ -3,7 +3,7 @@ yaml
 k: v
 """
 import yaml
-from path_tool import get_abs_path
+from utils.path_tool import get_abs_path
 
 
 def load_rag_config(config_path: str=get_abs_path("config/rag.yml"), encoding: str="utf-8"):
